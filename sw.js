2123bfc73383119599b3ca5bcbd9b1870c14a2f3
Obembe Godwin <obembegodwin@gmail.com>
2026-02-13 07:52:53 +0100

    decreased the font size on the receipt such that it accomodates input conviniently

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'pro-pos-v1';
+const CACHE_NAME = 'pro-pos-v3';
 const ASSETS = [
     './',
     './index.html',
@@ -12,6 +12,17 @@ const ASSETS = [
 self.addEventListener('install', event => {
     event.waitUntil(
         caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+    );
+    self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(keys => {
+            return Promise.all(
+                keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+            );
+        })
     );
 });
 
